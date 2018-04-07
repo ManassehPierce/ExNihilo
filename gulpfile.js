30485702123fe62bb587d4b58259adf241fa8193
Manasseh Pierce <manassehpierce@gmail.com>
2018-04-07 11:45:10 -0500

    alphadev06

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
+//
+
 var gulp    =    require('gulp');
 var babel   =    require('gulp-babel');
 var concat  =    require('gulp-concat');
 
 gulp.task('default', function() {
-	gulp.src(['src/custom.js', 'src/event.js', 'src/main.js', 'src/barrel.js', 'src/blocks.js'])
+	gulp.src(['polyfill/es6-shim.js', 'src/custom.js', 'src/event.js', 'src/main.js', 'src/barrel.js', 'src/blocks.js'])
 		.pipe(concat('ExNihiloPE.js'))
 		.pipe(babel({presets:['es2015']}))
 		.pipe(gulp.dest('dist'));
